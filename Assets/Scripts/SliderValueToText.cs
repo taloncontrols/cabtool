2af5ab1780a3d7a77309d98445d9fbbeb116068b
qinglu006 <qlu@taloncontrols.com>
2021-03-30 17:01:27 -0500

    slider notify

--- Assets/Scripts/SliderValueToText.cs
+++ Assets/Scripts/SliderValueToText.cs
@@ -1,8 +1,11 @@
 ﻿using Assets.Scripts;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SliderValueToText : MonoBehaviour
@@ -10,30 +13,64 @@ public class SliderValueToText : MonoBehaviour
     public Slider sliderUI;
     public string id;
     private Text textSliderValue;
-
+    private bool isBeingDragged = false;
+    
     void Start()
     {
-        //textSliderValue = GetComponent<Text>();
-        if (sliderUI!=null)
-        ShowSliderValue(sliderUI.value);
+        //sliderUI = GetComponent<Slider>();
+
+        if (sliderUI != null)
+            ShowSliderValue(sliderUI.value);
     }
 
-    public void ShowSliderValue(float value)
+    public void OnEndDrag()
     {
+        OnDrag();
+        //Debug.Log("drag end");
+        isBeingDragged = false;
+    }
+
+    public void OnBeginDrag()
+    {
+
+        //Debug.Log("drag start");
+
+        isBeingDragged = true;
+    }
+    public void OnDrag()
+    {
+        //Debug.Log("drag");
+
+        isBeingDragged = true;
+
+        if (sliderUI != null)
+            ShowSliderValue(sliderUI.value,true);
+    }
+
+    public void ShowSliderValue(float value, bool notify=false)
+    {
+      
         textSliderValue = GetComponent<Text>();
         //string sliderMessage = " " + sliderUI.value;
         if (textSliderValue != null)
         {
             textSliderValue.text = value.ToString("0");
+            if (notify)
             Change(value.ToString("0"));
         }
 
     }
 
+    public void ChangeSliderValue(string value)
+    {
+        sliderUI.value = string.IsNullOrWhiteSpace(value) ? 0f : (float)System.Convert.ToInt32(value);
+        ShowSliderValue(sliderUI.value);
+    }
+
     private void Change(string value)
     {
         GameObject Cupboard = GameObject.Find("Cupboard");
-        var cabinetService = Cupboard.GetComponent<CabinetService>();        
+        var cabinetService = Cupboard.GetComponent<CabinetService>();
         cabinetService.Change(id, value);
     }
 }
--- Assets/Scripts/Sliders.cs
+++ Assets/Scripts/Sliders.cs
@@ -11,7 +11,7 @@ public class Sliders : MonoBehaviour
     public int numSelectors = 5;
     public GameObject[] selectorArr;
     public GameObject selector; //selected in the editor
-    public Slider[] sliderArr;
+    //public Slider[] sliderArr;
     CabinetService cabinetService;
     void Start()
     {
@@ -39,7 +39,7 @@ public class Sliders : MonoBehaviour
             if (numSelectors > 0)
             {
                 selectorArr = new GameObject[numSelectors];
-                sliderArr = new Slider[numSelectors];
+                //sliderArr = new Slider[numSelectors];
                 for (int i = 0; i < numSelectors; i++)
                 {
                     var sensor = selected[i];
@@ -64,12 +64,14 @@ public class Sliders : MonoBehaviour
                         rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 100, sliderSize* slider.maxValue);
                         
                     }
+
+                    
                     //go.transform.parent = GameObject.Find("Canvas").transform;
                     go.transform.SetParent(GameObject.Find("Canvas").transform);
                     //go.transform.localScale = Vector3.one * 45;
                     //go.transform.localRotation = Quaternion.identity;
                     selectorArr[i] = go;
-                    sliderArr[i] = slider;
+                    //sliderArr[i] = slider;
                 }
             }
         }
@@ -94,10 +96,9 @@ public class Sliders : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
-
-   public void ChangeValue(IoItem sensor)
+   
+    public void ChangeValue(IoItem sensor)
     {
        
         bool found = false;
@@ -115,9 +116,14 @@ public class Sliders : MonoBehaviour
         }
         if (!found) return;
 
-       
-        var slider = sliderArr[i];
-        slider.value = string.IsNullOrWhiteSpace(sensor.Value) ? 0f : (float)System.Convert.ToInt32(sensor.Value);
+        //var slider = sliderArr[i];
+        //slider.value = string.IsNullOrWhiteSpace(sensor.Value) ? 0f : (float)System.Convert.ToInt32(sensor.Value);
+
+        var go = selectorArr[i];
+        var valueObject = go.transform.Find("Value").gameObject;
+        var sliderValueToText=valueObject.GetComponent<SliderValueToText>();
+        sliderValueToText.ChangeSliderValue(sensor.Value);
+        
         
     }
 }
